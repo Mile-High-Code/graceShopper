74d9b23740dae7831da5f2d788eaacbd34ee2d42
marinodomenick <marinodomenick@gmail.com>
2022-08-22 14:37:08 -0600

    Floors working again

--- src/api/items.js
+++ src/api/items.js
@@ -3,23 +3,23 @@ import axios from "axios";
 export const fetchAllItems = async () => {
   try {
     const { data } = await axios.get("/api/items");
-    return data
-  } catch(err) {
-    console.error(err)
+    return data;
+  } catch (err) {
+    console.error(err);
   }
 };
 
 export const fetchSingleItem = async (id) => {
   try {
     const { data } = await axios.get(`/api/items/${id}`);
-    return data
-  } catch(err) {
-    console.error(err)
+    return data;
+  } catch (err) {
+    console.error(err);
   }
 };
 
-export const fetchItemsByFloor = async (floorId) => {
-  const response = await fetch(`/api/items/floor/${floorId}`, {
+export const fetchItemsByFloor = async (id) => {
+  const response = await fetch(`/api/items/floor/${id}`, {
     headers: {
       "Content-Type": "application/json",
     },
